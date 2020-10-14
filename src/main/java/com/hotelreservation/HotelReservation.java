ee0f38efcc9dc71dd54513f5dc10ea6847e32981
ASHUTOSH <ashutosh@192.168.1.2>
2020-10-14 18:48:36 +0530

    [Ashutosh]Created:Welcome Message

---
+++ src/main/java/com/hotelreservation/HotelReservation.java
@@ -0,0 +1,11 @@
+package com.hotelreservation;
+
+public class HotelReservation {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Hotel Reservation System");
+
+	}
+
+}
